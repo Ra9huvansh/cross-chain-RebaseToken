ffa4c5dcb56eb5e59fe1ee44263036ba2682a04e
Raghuvansh <raghuvansh.siddharth004@gmail.com>
2025-08-05 18:39:48 +0530

    applyChainUpdates() configuration before burn/mint or ttransfer but after pool and token deployments done

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -6,7 +6,8 @@ import { RebaseToken } from "../src/RebaseToken.sol";
 import { RebaseTokenPool } from "../src/RebaseTokenPool.sol";
 import { Vault } from "../src/Vault.sol";
 import { IRebaseToken } from "../src/interfaces/IRebaseToken.sol";
-import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import { IERC20 } from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import { TokenPool } from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 
 // Import the Chainlink Local simulator
 import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
@@ -14,6 +15,8 @@ import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLo
 import {RegistryModuleOwnerCustom} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
 
 import {TokenAdminRegistry} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
+
+import {RateLimiter} from "@ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
 
 contract CrossChainTest is Test {
     uint256 sepoliaFork;
@@ -73,6 +76,9 @@ contract CrossChainTest is Test {
 
         vm.stopPrank(); // Stop impersonating
 
+        /*//////////////////////////////////////////////////////////////
+                         FETCHING NETWORK DETAILS
+        //////////////////////////////////////////////////////////////*/
 
         // Select Sepolia Fork
         vm.selectFork(sepoliaFork);
@@ -82,6 +88,9 @@ contract CrossChainTest is Test {
         vm.selectFork(arbSepoliaFork);
         arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid); 
 
+        /*//////////////////////////////////////////////////////////////
+                         INSTANTIATING TOKENPOOL
+        //////////////////////////////////////////////////////////////*/
 
         vm.selectFork(sepoliaFork); 
         sepoliaPool = new RebaseTokenPool(
@@ -99,6 +108,9 @@ contract CrossChainTest is Test {
             arbSepoliaNetworkDetails.routerAddress
         );
 
+        /*//////////////////////////////////////////////////////////////
+                       GRANTINGMINTANDBURNACCESS
+        //////////////////////////////////////////////////////////////*/
 
         // On Sepolia fork
         vm.selectFork(sepoliaFork);
@@ -113,6 +125,9 @@ contract CrossChainTest is Test {
         arbSepoliaToken.grantMintAndBurnRole(address(arbSepoliaPool));
         vm.stopPrank();
 
+        /*//////////////////////////////////////////////////////////////////
+        CLAIMING TOKEN ADMININSTRATOR ROLE THROUGH REGISTER ADMIN VIA OWNER
+        //////////////////////////////////////////////////////////////////*/
 
         // On Sepolia Fork
         vm.selectFork(sepoliaFork);
@@ -128,6 +143,9 @@ contract CrossChainTest is Test {
         .registerAdminViaGetCCIPAdmin(address(arbSepoliaToken));
         vm.stopPrank();
 
+        /*//////////////////////////////////////////////////////////////
+                 ACCEPTING TOKEN ADMINISTRATOR OR ADMIN ROLE
+        //////////////////////////////////////////////////////////////*/
 
         // On Sepolia fork
         vm.selectFork(sepoliaFork);
@@ -143,6 +161,9 @@ contract CrossChainTest is Test {
         .acceptAdminRole(address(arbSepoliaToken));
         vm.stopPrank();
 
+        /*//////////////////////////////////////////////////////////////
+                        LINK TOKEN TO POOL VIA ADMIN
+        //////////////////////////////////////////////////////////////*/
 
         // On Sepolia fork
         vm.selectFork(sepoliaFork);
@@ -157,5 +178,90 @@ contract CrossChainTest is Test {
         TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress)
         .setPool(address(arbSepoliaToken), address(arbSepoliaPool));
         vm.stopPrank();
+
+
+        /*//////////////////////////////////////////////////////////////
+                   ACTIVATING CROSS-CHAIN COMMUNICATION
+        //////////////////////////////////////////////////////////////*/
+
+        // Configure Sepolia Pool to interact with Arbitrum Sepolia Pool
+        configureTokenPool(
+            sepoliaFork,                            // Local chain: Sepolia
+            address(sepoliaPool),                   // Local pool: Sepolia's TokenPool
+            arbSepoliaNetworkDetails.chainSelector, // Remote chain selector: Arbitrum Sepolia's
+            address(arbSepoliaPool),                // Remote pool address: Arbitrum Sepolia's TokenPool
+            address(arbSepoliaToken)                // Remote token address: Arbitrum Sepolia's Token
+        );
+        
+        // Configure Arbitrum Sepolia Pool to interact with Sepolia Pool
+        configureTokenPool(
+            arbSepoliaFork,                         // Local chain: Arbitrum Sepolia
+            address(arbSepoliaPool),                // Local pool: Arbitrum Sepolia's TokenPool
+            sepoliaNetworkDetails.chainSelector,    // Remote chain selector: Sepolia's
+            address(sepoliaPool),                   // Remote pool address: Sepolia's TokenPool
+            address(sepoliaToken)                   // Remote token address: Sepolia's Token
+        );
     }
+
+
+
+
+
+    
+
+    // owner, sepoliaFork, arbSepoliaFork, sepoliaPool, arbSepoliaPool,
+    // sepoliaToken, arbSepoliaToken, sepoliaNetworkDetails, arbSepoliaNetworkDetails
+    // are assumed to be defined elsewhere in your test setup.
+
+    function configureTokenPool(
+        uint256 forkId, // The fork ID of the local chain
+        address localPoolAddress, // Address of the pool being configured
+        uint64 remoteChainSelector, // Chain selector of the remote chain
+        address remotePoolAddress, // Address of the pool on the remote chain
+        address remoteTokenAddress // Address of the token on the remote chain
+    ) public {
+        // 1. Select the correct fork (local chain context)
+        vm.selectFork(forkId);
+
+        // 2. Prepare arguments for applyChainUpdates
+        // An empty array as we are only adding, not removing.
+        // uint64[] memory remoteChainSelectorsToRemove = new uint64[](0);
+
+        // Construct the chainsToAdd array (with one ChainUpdate struct)
+        TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+
+        // The remote pool address needs to be ABI-encoded as bytes.
+        // CCIP expects an array of remote pool addresses, even if there's just one primary.
+        bytes[] memory remotePoolAddressesBytesArray = new bytes[](1);
+        remotePoolAddressesBytesArray[0] = abi.encode(remotePoolAddress);
+
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            allowed: true,
+            remotePoolAddress: abi.encode(remotePoolAddressesBytesArray), // ABI-encode the array of bytes
+            remoteTokenAddress: abi.encode(remoteTokenAddress),
+            // For this example, rate limits are disabled.
+            // Consult CCIP documentation for production rate limit configurations.
+            outboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: false,
+                capacity: 0,
+                rate: 0
+            }),
+            inboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: false,
+                capacity: 0,
+                rate: 0
+            })
+        });
+
+        // 3. Execute applyChainUpdates as the owner
+        // applyChainUpdates is typically an owner-restricted function.
+        vm.prank(owner); // The 'owner' variable should be the deployer/owner of the localPoolAddress
+        TokenPool(localPoolAddress).applyChainUpdates(
+            // remoteChainSelectorsToRemove,
+            chainsToAdd
+        );
+    }
+
+
 }
